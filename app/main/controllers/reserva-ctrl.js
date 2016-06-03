e1841af899f591a95d45decd5eab8880d7ac60d8
Thiago Antune <thiago.antunes@outlook.com>
2016-06-03 18:29:44 -0300

    Horarios Livres

--- app/main/controllers/reserva-ctrl.js
+++ app/main/controllers/reserva-ctrl.js
@@ -1,17 +1,72 @@
+/*global _ moment*/
 'use strict';
 angular.module('main')
   .controller('ReservaCtrl', function (quadras, ReservasService, $stateParams) {
     var vm = this;
+    vm.intervaloSelecionado = getIntervaloDia(new Date());
+    vm.horariosPorQuadra = [];
     vm.quadras = quadras;
-    var intervaloData = getIntervaloDia(new Date);
-    vm.reservas = ReservasService.getReservasDia($stateParams.id, intervaloData.start, intervaloData.end);
+    vm.reservas = ReservasService.getReservasDia($stateParams.id, vm.intervaloSelecionado.start, vm.intervaloSelecionado.end);
+
+    activate();
+
+    function activate () {
+      vm.reservas.$loaded().then(getHorariosLivres);
+    }
+
+    function getHorariosLivres () {
+      _.forEach(vm.quadras, function (quadra) {
+        var horarios = getHorariosDia(quadra);
+        vm.horariosPorQuadra.push({
+          quadra: quadra,
+          horarios: horarios
+        });
+      });
+    }
+
+    function getHorariosDia (quadra) {
+      var diaSemana = moment(vm.intervaloSelecionado.start)._d.getDay() + '';
+      var func = _.orderBy(_.filter(quadra.funcionamento, { dow: diaSemana }), 'start', 'asc');
+      var horariosLivres = [];
+
+      _.forEach(func, function (f) {
+        var start = moment(f.start, 'hh:mm');
+        var end = start.add(1, 'h');
+
+        while (end <= moment(f.end, 'hh:mm')) {
+          var horario = {
+            start: start / 1,
+            end: end / 1
+          };
+
+          var horarioLivre = _.every(vm.reservas, function (r) {
+            return !(
+                r.start === horario.start ||
+                r.end === horario.end ||
+                r.start < horario.start && horario.start < horario.end ||
+                r.start > horario.start && horario.end > r.start
+              );
+          });
+
+          if (horarioLivre) {
+            horariosLivres.push(horario);
+          }
+
+          start = start.add(1, 'h');
+          end = end.add(1, 'h');
+        }
+
+      });
+      return horariosLivres;
+    }
 
     function getIntervaloDia (date) {
       var startOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
       var endOfDay = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
       return {
-        start: startOfDay / 1000,
-        end: endOfDay / 1000
+        start: startOfDay / 1,
+        end: endOfDay / 1
       };
     }
+
   });
